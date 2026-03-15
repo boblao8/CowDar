77f579e86d86ed44c1daf922419f6ffd31a1e7c6
aadidapurkar <aadidapurkar04@gmail.com>
2026-03-15 14:06:06 +1100

    .

--- ios-app/SharedModels.swift
+++ ios-app/SharedModels.swift
@@ -3,7 +3,7 @@ import SwiftUI
 
 // CapturedPayload is unique to the camera flow and is NOT defined in ContentView.swift.
 // Make it Identifiable so it can be used with .navigationDestination(item:) and fullScreenCover(item:).
-struct CapturedPayload: Identifiable {
+struct CapturedPayload: Identifiable, Equatable {
     let id = UUID()
     let depthData: Data
     let depthWidth: Int
